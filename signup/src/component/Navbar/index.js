dc8191eb635ef6f5ef844bffc1fe904a16e57d0e
Gaurav Singh <gauravsingh.info@gmail.com>
2021-07-25 15:09:18 +0530

    removed unnecessary files, re-structure acoording to requirement

---
+++ signup/src/component/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function Navbar() {
+    return (
+        <div>
+            <h1 className="logo text-center mt-3 text-white">Omega</h1>
+        </div>
+    )
+}
+
+export default Navbar
